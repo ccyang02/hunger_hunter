671f6eaba5022775e7e35dd5ce8b4a8cbdf9dda1
ccyang02 <chiachingyang02@gmail.com>
2020-09-28 09:38:15 +0800

    Fix: a bug for searching keyword

--- app.js
+++ app.js
@@ -16,15 +16,18 @@ app.get('/', (req, res) => {
 
 app.get('/restaurants/:rest_id', (req, res) => {
   const restaurant = data.results.find(element => element.id.toString() === req.params.rest_id)
-  res.render('show', { restaurant: restaurant })
+  res.render('show', { restaurant })
 })
 
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = data.results.filter(element => JSON.stringify(element).includes(req.query.keyword))
-  // how to speed up when there are a lot of data?
-
-  res.render('index', { restaurants: restaurants, keyword })
+  const keyword = req.query.keyword.trim().toLowerCase()
+  const restaurants = data.results.filter(element => {
+    return element.name.toLowerCase().includes(keyword) ||
+      element.category.toLowerCase().includes(keyword) ||
+      element.location.toLowerCase().includes(keyword) ||
+      element.description.toLowerCase().includes(keyword)
+  })
+  res.render('index', { restaurants, keyword })
 })
 
 app.listen(port, () => {
