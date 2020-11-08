eacdaacc68c654ab0a0d438a8ef76946acfa6c78
ccyang02 <chiachingyang02@gmail.com>
2020-11-08 15:30:53 +0800

    feat: add logout feature

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -33,6 +33,9 @@ router.post('/register', (req, res) => {
     })
 })
 
-router.get('/logout', (req, res) => { })
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
 
 module.exports = router
